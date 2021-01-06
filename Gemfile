47020521e5b5f5aa426edcf6ffe90153276d96e9
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-05 21:51:20 -0800

    update formtastic and make thigns work

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'activeadmin'
+gem 'formtastic', '4.0.0.rc1'
 
 #######
 # Search
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
