4e6ed9e92bf3d49418f1afbc35155d6e5195da0a
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-11 08:43:06 -0800

    add actors relationship

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,6 +1,10 @@
 class Title < ApplicationRecord
-  has_many :appearances
+  has_many :appearances, inverse_of: :title, dependent: :destroy
   has_many :participants, through: :appearances
-  has_many :directors_apperances, -> { where(role: :director) }, class_name: 'Appearance'
-  has_many :directors, through: :directors_apperances, class_name: 'Participant', source: :participant
+
+  has_many :appearances_by_directors, -> { where(role: :director) }, class_name: 'Appearance', inverse_of: :title
+  has_many :directors, through: :appearances_by_directors, class_name: 'Participant', source: :participant
+
+  has_many :appearances_by_actors, -> { where(role: :actor) }, class_name: 'Appearance', inverse_of: :title
+  has_many :actors, through: :appearances_by_actors, class_name: 'Participant', source: :participant
 end
--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -3,6 +3,31 @@ require 'rails_helper'
 RSpec.describe Title, type: :model do
   let(:title) { create :title }
 
+  describe '#participants' do
+    subject { title.participants }
+
+    context 'with no participants' do
+      it { is_expected.to match_array([]) }
+    end
+
+    context 'when one participants' do
+      let!(:participant) { create :participant }
+      let!(:appearance) { create :appearance, title: title, participant: participant }
+
+      it { is_expected.to match_array([participant]) }
+    end
+
+    context 'with one director and one actor' do
+      let!(:director) { create :participant }
+      let!(:appearance_1) { create :appearance, title: title, participant: director, role: :director }
+
+      let!(:actor) { create :participant }
+      let!(:appearance_2) { create :appearance, title: title, participant: actor, role: :actor }
+
+      it { is_expected.to match_array([director, actor]) }
+    end
+  end
+
   describe '#directors' do
     subject { title.directors }
 
@@ -38,18 +63,18 @@ RSpec.describe Title, type: :model do
     end
   end
 
-  describe '#participants' do
-    subject { title.participants }
+  describe '#actors' do
+    subject { title.actors }
 
-    context 'with no participants' do
+    context 'with no directors' do
       it { is_expected.to match_array([]) }
     end
 
-    context 'when one participants' do
-      let!(:participant) { create :participant }
-      let!(:appearance) { create :appearance, title: title, participant: participant }
+    context 'with one director' do
+      let!(:actor) { create :participant }
+      let!(:appearance) { create :appearance, title: title, participant: actor, role: :actor }
 
-      it { is_expected.to match_array([participant]) }
+      it { is_expected.to match_array([actor]) }
     end
 
     context 'with one director and one actor' do
@@ -59,7 +84,17 @@ RSpec.describe Title, type: :model do
       let!(:actor) { create :participant }
       let!(:appearance_2) { create :appearance, title: title, participant: actor, role: :actor }
 
-      it { is_expected.to match_array([director, actor]) }
+      it { is_expected.to match_array([actor]) }
+    end
+
+    context 'with many actors' do
+      let!(:actor_1) { create :participant }
+      let!(:appearance_1) { create :appearance, title: title, participant: actor_1, role: :actor }
+
+      let!(:actor_2) { create :participant }
+      let!(:appearance_2) { create :appearance, title: title, participant: actor_2, role: :actor }
+
+      it { is_expected.to match_array([actor_1, actor_2]) }
     end
   end
 end
